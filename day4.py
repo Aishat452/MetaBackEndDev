3330879cc988dd7c72a672b902df87fbf40b66ff
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-04-29 07:12:09 +0100

    Create day4.py

---
+++ day4.py
@@ -0,0 +1,14 @@
+#Global scope 
+
+special = 5
+def get_total(a, b):
+    #enclosed scope variable declared inside a function
+    total = a + b
+    print(special)
+    def double_it():
+        #local variable
+        double = total * 2
+        print(special)
+
+    double_it()
+    return total
