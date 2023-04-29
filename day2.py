a67916bc1c2f6009f05b1e46ed09d1983219c852
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-04-29 07:05:10 +0100

    Update day2.py

---
+++ day2.py
@@ -0,0 +1,11 @@
+#local scope 
+
+def get_total(a, b):
+    #local variable declared inside a function
+    total = a + b;
+    return total
+print(get_total(5, 2))
+
+# Accessing variable outside of the function:
+print(total)
+NameError: name 'total' is not defined
